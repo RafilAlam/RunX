d01b608d71e27a464ac1f8bfaf90e2687857abb8
Rafil Alam <rafilalam009@gmail.com>
2026-02-08 21:47:18 +0800

    added remove command and renamed reg to add

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,35 @@
 #include <string>
 #include <map>
 
-std::map<std::string, std::string> mapfile(std::string filepath) {
-  std::map<std::string, std::string> map;
+typedef std::map<std::string, std::string> configmap;
+
+configmap file_to_map(std::string filepath) {
+  configmap map;
   std::ifstream file(filepath);
   std::string name;
   std::string path;
   while (std::getline(file, name, '=') && std::getline(file, path)) {
     map[name] = path;
   }
+  file.close();
   return map;
+}
+
+void map_to_file(configmap map, std::string filepath) {
+  std::ofstream file(filepath);
+  for (const auto& pair : map) {
+    file << pair.first << '=' << pair.second << std::endl;
+  }
+  file.close();
 }
 
 std::map<std::string, std::function<void(const char**)>> functions;
 
-void init() {
+void init(int argc) {
   std::string path = std::getenv("HOME") + std::string("/.config/runx/");
   std::filesystem::create_directory(path);
 
-  functions["reg"] = [path](const char* argv[]) {
+  functions["add"] = [path](const char* argv[]) {
     std::ofstream file(path + "runx.conf", std::ios::app);
     if (!file) {
       std::cerr << "Unable to load config file!\n";
@@ -35,12 +46,20 @@ void init() {
   };
 
   functions["run"] = [path](const char* argv[]) {
-    std::map<std::string, std::string> map = mapfile(path + "runx.conf");
+    configmap map = file_to_map(path + "runx.conf");
     std::system(map[argv[2]].c_str());
+  };
+
+  functions["rm"] = [path, argc](const char* argv[]) {
+    configmap map = file_to_map(path + "runx.conf");
+    for (int i=2; i<argc; ++i) {
+      map.erase(argv[i]);
+    }
+    map_to_file(map, path + "runx.conf");
   };
 }
 
 int main(int argc, const char* argv[]) {
-  init();
+  init(argc);
   functions[argv[1]](argv);
 }
